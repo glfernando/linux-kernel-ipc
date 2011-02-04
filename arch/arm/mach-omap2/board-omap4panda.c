e5b791682dcd9c4d1091f2d1191b1d5636033ed6
Dima Zavin <dima@android.com>
2011-02-04 14:50:52 -0800

    ARM: omap2: panda: add basic android init (adb)
    Change-Id: Ifdd03aaeb56063345131bc40a7a6d1ebecd62a3b
    Signed-off-by: Dima Zavin <dima@android.com>

--- arch/arm/mach-omap2/board-omap4panda.c
+++ arch/arm/mach-omap2/board-omap4panda.c
@@ -718,6 +718,8 @@ void omap4_panda_display_init(void)
 	omap_display_init(&omap4_panda_dss_data);
 }
 
+extern void __init omap4_panda_android_init(void);
+
 static void __init omap4_panda_init(void)
 {
 	int package = OMAP_PACKAGE_CBS;
@@ -736,7 +738,10 @@ static void __init omap4_panda_init(void)
 	omap4_twl6030_hsmmc_init(mmc);
 	omap4_ehci_init();
 	usb_musb_init(&musb_board_data);
+
 	omap4_panda_display_init();
+
+	omap4_panda_android_init();
 }
 
 static void __init omap4_panda_map_io(void)
